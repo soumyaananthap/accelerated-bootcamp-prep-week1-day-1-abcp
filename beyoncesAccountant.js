30ed42512a854f1b27f4fc8b6548204c3adcb08d
Soumya Anantha Padmanabha <soumya_anantha@yahoo.com>
2018-01-30 06:53:50 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -3,9 +3,8 @@ var beyoncesMoney = totalSum;
 console.log("Dear Beyonce,")
 console.log("We have calculated your total earnings this year.")
 console.log("You have earned " + beyoncesMoney)
-console.log("Of" + totalSum + " some portion derives from your live performances.")
-console.log("Of" + totalSum + " some portion derives from music sales.")
-return totalSum;
+console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from your live performances.")
+console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
 
 
 // DON'T DO THIS PART YET:
